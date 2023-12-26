f1c79f256f2a77b9810a7611a5fc0c800cbbd5a2
Pavel Koryagin <pavel.koryagin.76@bk.ru>
2023-12-26 21:30:44 +0800

    write function modalFunc

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log(document);
+const modalFunc = () => {
+  const modal = document.querySelector('.card-modal__overlay');
+const cardBtn = document.querySelector('#card-button');
+
+
+ const openModal = () => {
+  modal.classList.add('open')
+ }
+
+ const closeModal = () => {
+  modal.classList.remove('open')
+ }
+
+cardBtn.addEventListener( 'click', () => {
+ openModal();
+})
+
+ modal.addEventListener('click', (event) => {
+ 
+  if(event.target.classList.contains('card-modal__overlay') || 
+      event.target.closest('.card-modal__header--close')){
+    closeModal();
+  }
+  
+ });
+};
+modalFunc();
